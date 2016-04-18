7f239761d4a6b39bdfa6024515c94fb0a09b0147
Xiaofei-Zhang <Xiaofei@Xiaofeis-MacBook-Pro-3.local>
2016-04-18 00:03:07 -0400

    response function added

--- learn.py
+++ learn.py
@@ -202,6 +202,22 @@ def execute(query):
 			outcome.append('UNSURE')
 
 	return response(outcome)
-	
+
+# return the prediction string
 def response(outcome):
-	return "The diagnosis is ", outcome[0], "Also, ", str(outcome[1:])
+	if outcome[0] == 'UNSURE':
+		return "Your input doesn't have enough information for a precise prediction."
+	elif outcome[0] == '':
+		return "Your input doesn't have enough information for the cancer site prediction."
+	elif outcome[0] == 'OTHER':
+		return "The cancer site is other than BREAST, LUNG, PROSTATE, CRC."
+	elif outcome[0] == 'BEAST':
+		return "The cancer site is BREAST. Estrogen receptor is ", str(outcome[1])," and progesterone receptor is ", str(outcome[2]), "."
+	elif outcome[0] == 'LUNG':
+		return "The cancer site is LUNG. Cancer cell type is ", str(outcome[3])
+	elif outcome[0] == 'CRC':
+		return "The cancer site is COLON or RECTUM. Carcinoembryonic antigen result is ", str(outcome[4])
+	else:
+		return "The cancer site is PROSTATE"
+
+#return "The diagnosis is ", outcome[0], "Also, ", str(outcome[1:])
