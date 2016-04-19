dc939103caf73d1d5498cea37fce17ae731c79f8
Xiaofei-Zhang <Xiaofei@Xiaofeis-MacBook-Pro-3.local>
2016-04-18 21:45:29 -0400

    response function updated

--- learn.py
+++ learn.py
@@ -210,14 +210,34 @@ def response(outcome):
 	elif outcome[0] == '':
 		return "Your input doesn't have enough information for the cancer site prediction."
 	elif outcome[0] == 'OTHER':
-		return "The cancer site is other than BREAST, LUNG, PROSTATE, CRC."
+		return "The report shows that the cancer site is other than BREAST, LUNG, PROSTATE, CRC."
 	elif outcome[0] == 'BREAST':
-		return "The cancer site is BREAST. Estrogen receptor is " + str(outcome[1]) + " and progesterone receptor is " + str(outcome[2]) + "."
+		str1= "The report indicates the possibility of BREAST cancer. "
+		if outcome[1] != 'UNSURE' and outcome[2] != 'UNSURE':
+			str2="The report test "+ str(outcome[1]) + " for estrogen receptor and " + str(outcome[2]) + " for progesterone receptor."
+		elif outcome[1] =='UNSURE' and outcome[2] != 'UNSURE': 
+			str2="The report test "+ str(outcome[2]) + " for progesterone receptor. However, no precise prediction was made for estrogen receptor."
+		elif outcome[1] !='UNSURE' and outcome[2] == 'UNSURE':
+			str2="The report test "+ str(outcome[2]) + " for estrogen receptor. However, no precise prediction was made for progesterone receptor."
+		else:
+			str2="However, the information in you input is not enough for a precise hormone receptor status prediction."
+			return str1 + str2
 	elif outcome[0] == 'LUNG':
-		return "The cancer site is LUNG. Cancer cell type is " + str(outcome[3])
+		str1= "The report indicates the possibility of LUNG cancer. "
+		if outcome[3] == 'UNSURE':
+			str2 = "No precise cell type prediction was made."
+		elif outcome[3] == 'OTHER':
+			str2 = "The report shows the cancer is neither squamous cell carcinoma nor adenocarcinoma."
+		else: 
+			str2 = "The report shows the cancer cell type is " + str(outcome[3]) + "."
+		return str1 + str2
 	elif outcome[0] == 'CRC':
-		return "The cancer site is COLON or RECTUM. Carcinoembryonic antigen result is " + str(outcome[4]) + "."
+		str1= "The report indicates the possibility of COLORECTAL cancer. "
+		if outcome[4]=='UNSURE':
+			str2="No precise prediction was made for carcinoembryonic antigen."
+		else:
+			str2-"The report shows the carcinoembryonic antigen status is " + str(outcome[4]) + "."
+		return str1 + str2
 	else:
-		return "The cancer site is PROSTATE."
+		return "The report indicates the possibility of PROSTATE cancer. "
 
-#return "The diagnosis is ", outcome[0], "Also, ", str(outcome[1:])
